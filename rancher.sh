2eec309c026b57a6a98b683159722b05c316e65b
HEBERTSON MENEZES <32960000+hebertsonm@users.noreply.github.com>
2019-01-14 21:21:05 -0800

    Update rancher.sh

--- rancher.sh
+++ rancher.sh
@@ -3,10 +3,8 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-apt-cache madison docker-ce
-sudo apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial
-sudo docker container run hello-world
-sudo usermod -aG docker hebertson
+sudo apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
 sudo systemctl enable docker
+sudo usermod -aG docker rancher-admin
 bash -l
 docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher:latest --acme-domain rancheradmin.westus2.cloudapp.azure.com
