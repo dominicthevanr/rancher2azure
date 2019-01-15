583a8c18ca61805ab75c7500bf85378cc9eac3ac
HEBERTSON MENEZES <32960000+hebertsonm@users.noreply.github.com>
2019-01-14 21:13:33 -0800

    Update rancher.sh

--- rancher.sh
+++ rancher.sh
@@ -1,10 +1,7 @@
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-	sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	$(lsb_release -cs) \
-	stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache madison docker-ce
 sudo apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial
